885b44547ff514102f496c6431274fddea957d37
oneoopss <artemka212_1998@mail.ru>
2023-03-29 00:48:20 +0500

    29.03

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,4 @@ class Blog(models.Model):
     title = models.CharField(max_length=100)
     discription = models.CharField(max_length=200)
     text = models.TextField()
+    date = models.DateField(auto_now=False, auto_now_add=False,)
--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Blog
 
 def all_blogs(r):
-    Blogs = Blog.objects.all()
-    return render(r, 'blog/all_blogs.html', {'Blogs':Blogs})
+    blogs = Blog.objects.order_by('-date')[:2]
+    return render(r, 'blog/all_blogs.html', {'blogs':blogs})
