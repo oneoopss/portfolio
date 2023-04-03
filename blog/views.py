260de179fbf1e8f9ef7a1c8afe1c2d170f40aca3
oneoopss <artemka212_1998@mail.ru>
2023-04-03 22:03:56 +0500

    последнее обновление

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def all_blogs(r):
-    blogs = Blog.objects.order_by('-date')[:2]
+    blogs = Blog.objects.order_by('-date')
     return render(r, 'blog/all_blogs.html', {'blogs':blogs})
+
+def detail(r, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(r, 'blog/detail.html',{'blog':blog})
